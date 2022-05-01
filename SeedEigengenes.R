32794f6e2365cf11e7e3a26ff41fea55a5f0e252
p100mma <p10mma_gh@protonmail.com>
2022-05-01 21:00:12 +0200

    Started writing another tools

--- SeedEigengenes.R
+++ SeedEigengenes.R
@@ -1,5 +1,5 @@
 library(MASS)
-
+library(matrixcalc)
 layered_sim2EigenCors<-function(layered_sim, ...)
 {
     " Create correlation matrices between eigengenes nested in layered_sim object,
@@ -12,3 +12,14 @@ layered_sim2EigenCors<-function(layered_sim, ...)
     }
     return(clist)
 }
+
+Cormat2Eigengenes<-function(ref_cor, n_samples) 
+{
+    if (!is.positive.definite(ref_cor)) stop("input correlation matrix should be positive definite")
+    EIDF=as.data.frame(mvrnorm(n_samples, mu= rep(0, ncol(ref_cor)), Sigma=ref_cor))
+    if (!is.null(colnames(ref_cor))) colnames(EIDF)<- colnames(ref_cor)
+    return(EIDF)
+
+}
+
+layered_sim2
