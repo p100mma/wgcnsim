711b6b30a6434d6b7e1a3cf184b65a3462d0f8e7
p100mma <p10mma_gh@protonmail.com>
2022-05-04 03:29:47 +0200

    started work on automatic saving of clusterings

--- WGCNA_clust_utils.R
+++ WGCNA_clust_utils.R
@@ -1,10 +1,17 @@
 
-powerEstimation <-function(corMatrix, Rcut=0.85, powers=c(1:8), hist_breaks=10){
-   pickSoftThreshold.fromSimilarity(
+powerEstimation <-function(corMatrix, Rcut=0.85, powers=c(1:8), hist_breaks=10, save=TRUE,
+                           datasets_path=NULL, dataset_name=NULL, #relevant if save=TRUE
+                            ...){
+  result<-pickSoftThreshold.fromSimilarity(
   similarity=abs(corMatrix),
   RsquaredCut=Rcut,
   powerVector=c(1:8),
-  nBreaks=hist_breaks)
+  nBreaks=hist_breaks,...)
+  if (save)
+    {
+        saveRDS(result, paste0(datasets_path,'/',dataset_name,'/networks/',result$powerEstimate,'.rds'))
+    }
+    return(result)
 }
 
 dissTOM2dendro <- function (distance_matrix, method="average"){
@@ -26,13 +33,18 @@ colors2MEs <- function (clabels, dE, ...) {
 #Starts with adjacency matrix (i.e. output from adjacency.fromSimilarity from WGCNA)
 #calculation of TOM in case of diss_as_TOM=TRUE is compute-intensive
 #if that's the problem consider using ClusteringResults.fromSimilarity
-ClusteringResults.fromAdjacency <- function ( adj_mat, dataExpr, save_steps=FALSE, save_final=TRUE,
-                                             diss_as_TOM= TRUE,ts_other_args=NULL,
-                                             hclust_method = "average", minModSize=30,
+ClusteringResults.fromAdjacency <- function ( adj_mat, dataExpr, save_steps=FALSE, save_final=FALSE,
+                                              diss_as_TOM= TRUE,ts_other_args=NULL,
+                                              hclust_method = "average", minModSize=30,
                                               dc_sensivity= 2, dc_pam2parent=FALSE, dc_doPAM = TRUE, 
                                               verbose_lvl=2, dc_other_args=NULL, ME_other_args= NULL,
-                                             calculateMEs=TRUE){
-  
+                                              calculateMEs=TRUE,
+                                              datasets_path=NULL, dataset_name=NULL, network_name=NULL,
+                                              new_clustering_name=NULL,
+                                              save_args=FALSE) #relevant if save=TRUE
+  if (!is.null(datasets_path)){
+    if (is.null(dataset_name) | is.null(network_name)| is.null(  new_clustering_name )) stop('provide dataset_name, new_clustering_name and network_name arguments if datasets_path is not null')
+    prefix_path=paste0(datasets_path,'/',dataset_name,'/',network_name,'/')}else{prefix_path=NULL}
   
   if (verbose_lvl>0) print("Calculating dissimilarity...")
   if (diss_as_TOM){
@@ -47,7 +59,7 @@ ClusteringResults.fromAdjacency <- function ( adj_mat, dataExpr, save_steps=FALS
   
   if (verbose_lvl>0) print("Calculated dissimilarity")
   
-  if (save_steps) {if (verbose_lvl>0) print("Saving dissimilarity matrix..."); saveRDS(dissTOM, "dissMat.rds");
+  if (save_steps) {if (verbose_lvl>0) print("Saving dissimilarity matrix..."); saveRDS(dissTOM, paste0(  prefix_path,"dissMat.rds") )
                    if (verbose_lvl>0) print("Saved dissimilarity matrix to dissMat.rds")}
   
   if (verbose_lvl>0) print("Making dendogram...")
@@ -55,7 +67,7 @@ ClusteringResults.fromAdjacency <- function ( adj_mat, dataExpr, save_steps=FALS
   
   if (verbose_lvl>0) print("Finished dendogram.")
   
-  if (save_steps) {if (verbose_lvl>0) print("Saving dendrogram..."); saveRDS(dendogram, "geneTree.rds");
+  if (save_steps) {if (verbose_lvl>0) print("Saving dendrogram..."); saveRDS(dendogram, paste0(  prefix_path,"geneTree.rds") )
     if (verbose_lvl>0) print("Saved dendogram to geneTree.rds")}
   
   if (verbose_lvl>0) print("Calculating cluster labels...")
@@ -69,7 +81,7 @@ ClusteringResults.fromAdjacency <- function ( adj_mat, dataExpr, save_steps=FALS
   
   if (verbose_lvl>0) print("Finished clustering.")
   
-  if (save_steps) {if (verbose_lvl>0) print("Saving cluster labels..."); saveRDS(dynamicMods, "dynamicMods.rds");
+  if (save_steps) {if (verbose_lvl>0) print("Saving cluster labels...");if (!is.null ( dataset_name )) saveRDS(dynamicMods, prefix_path,'/clusterings/',new_clustering_name,"/dynamicMods.rds") else saveRDS(dynamicMods,'dynamicMods.rds');
     if (verbose_lvl>0) print("Saved cluster labels to dynamicMods.rds")}
   
   
@@ -85,8 +97,6 @@ ClusteringResults.fromAdjacency <- function ( adj_mat, dataExpr, save_steps=FALS
   
   if (verbose_lvl>0) print("Calculated Eigengenes.")
   
-  if (save_steps) {if (verbose_lvl>0) print("Saving module eigengene data..."); saveRDS(dynamicMods, "ME_list.rds");
-    if (verbose_lvl>0) print("Saved module eigengene data to ME_list.rds")}
   } else{
   ME_list <- "X"}
   
@@ -95,14 +105,37 @@ ClusteringResults.fromAdjacency <- function ( adj_mat, dataExpr, save_steps=FALS
                         ME_data = ME_list
                         )
   
-  if (save_final) {if (verbose_lvl>0) print("Saving final results (dendogram, color labels, MEs..."); saveRDS(final_results, "Adj2Clust_results.rds");
+  if (save_final) {if (verbose_lvl>0) print("Saving final results (dendogram, color labels, MEs..."); 
+    if (!is.null ( dataset_name )) saveRDS(final_results, prefix_path,'/clusterings/',new_clustering_name,"/Adj2Clust_results.rds")
+else saveRDS(final_results, "Adj2Clust_results.rds");
     if (verbose_lvl>0) print("Saved final results to Adj2Clust_results.rds")}
-  
-  return(final_results)
+  if (save_args)  (!is.null ( dataset_name )) saveRDS( list(  
+                                                              diss_as_TOM= diss_as_TOM,ts_other_args=ts_other_args,
+                                                              hclust_method = hclust_method, minModSize=minModSize,
+                                                              dc_sensivity= dc_sensivity, dc_pam2parent=dc_pam2parent, dc_doPAM = dc_doPAM, 
+                                                              dc_other_args=dc_other_args,  ME_other_args= ME_other_args,
+                                                              ),
+                                                                prefix_path,'/clusterings/',new_clustering_name,"/Adj2Clust_args.rds")
+
+return(final_results)}
+DoClusterFromFilenameArgs<- function(
+                                     datasets_path, dataset_name, network_name,clustering_name, method_cor='spearman',
+                                     expr_data=NULL, expr_data_path=NULL, has_decision=FALSE, expr_RData=TRUE){
+prefix_path=paste0(datasets_path,'/',dataset_name,'/',network_name,'/')
+ARGS<- readRDS(prefix_path,'/clusterings/',clustering_name,"/Adj2Clust_args.rds")
+ARGS$save_final=FALSE
+if (is.null(expr_data)){ if (is.null(expr_data_path)) stop('expr_data or expr_data_path must not be null') 
+                                                      else if (expr_RData) { load(expr_data_path); expr_data<-as.matrix(data.train) } 
+                                                                            else  expr_data<- readRDS(expr_data_path)
+                        }
+if (has_decision) expr_data<- expr_data[,-1]
+C<- cor(expr_data, method_cor)
+ARGS$adj_mat <-abs(C)^network_name
+ARGS$dataExpr<- expr_data
+RESULT<- do.call(ClusteringResults.fromAdjacency, ARGS)
+return(RESULT)
 }
-
 #Here similarity is meant as a measure of clustering
-
 ClusteringResults.fromSimilarity<- function(sim_mat, dataExpr, save_steps=FALSE, save_final=TRUE,
                                  hclust_method = "average", minModSize=30,
                                  dc_sensivity= 2, dc_pam2parent=FALSE, dc_doPAM = TRUE, 
---
+++ folder_tree.sh
@@ -0,0 +1,8 @@
+
+mkdir -p $1/datasets/$2
+mkdir $1/datasets/$2/networks
+mkdir $1/datasets/$2/clusterings
+mkdir -p $1/datasets/$2/simulations/base
+mkdir $1/datasets/$2/simulations/hierarchical
+mkdir $1/datasets/$2/simulations/GraySubmodules
+mkdir $1/datasets/$2/simulations/Final
